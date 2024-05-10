0ac1eae958d6852394f31c85e75e241b326f4162
ErickST00N <erickboys37@gmail.com>
2024-05-10 08:42:01 -0600

    Initial commit

---
+++ src/main/java/com/erickst00n/cajeroatm/CajeroATM.java
@@ -0,0 +1,20 @@
+/**
+ * Hacer un programa que simule un cajero automatico
+ * Debe consultar saldo
+ * Retirar efectivo
+ * Deposito de efectivo
+ * Salir
+ */
+
+package com.erickst00n.cajeroatm;
+
+/**
+ *
+ * @author erickstoon
+ */
+public class CajeroATM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
